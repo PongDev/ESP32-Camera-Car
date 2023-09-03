eb4be035efcc37be06451e026af905b93dd977d7
PongDev <pongwiwat.dev@gmail.com>
2023-09-04 06:31:03 +0700

    feat: draft uuid system

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,25 +1,41 @@
 import express, { Request } from "express";
 import expressWs from "express-ws";
 import ws from "ws";
-import fs from "fs";
+import { v4 as uuidv4 } from "uuid";
 
 const app = expressWs(express()).app;
+
+const streamWebSocket: {
+  [key: string]: ws;
+} = {};
 
 app.ws("/", (ws: ws, req: Request) => {
   ws.on("message", (msg) => {
     const buffer = msg as Uint8Array;
-    console.log(`Received message len: ${(msg as Buffer).length}`);
+    // console.log(`Received message len: ${(msg as Buffer).length}`);
 
     switch (buffer.at(0)) {
       case 1:
-        // fs.writeFileSync(`tmp/${Date.now()}.jpg`, buffer.slice(1) as Buffer);
-        ws.send("Received image");
+        for (const key in streamWebSocket) {
+          streamWebSocket[key].send(buffer.slice(1));
+        }
+        // ws.send("Received image");
         break;
       default:
-        console.log("Unknown message type");
+        console.error("Unknown message type");
     }
   });
   ws.send("Connected!");
+});
+
+app.ws("/stream", (ws: ws, req: Request) => {
+  const uuid = uuidv4();
+
+  streamWebSocket[uuid] = ws;
+
+  ws.on("close", (code: number, reason: Buffer) => {
+    delete streamWebSocket[uuid];
+  });
 });
 
 app.listen(3000);
